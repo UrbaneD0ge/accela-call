e23d0dfb0cb2215599d6b75cf24e11f46ea767c8
Kip Dunlap <kipling.dunlap@gmail.com>
2023-05-03 13:41:34 -0400

    successful call

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('button').addEventListener('click', function () {
+  // alert('Hello World!');
+  fetch('https://apis.accela.com/v4/records', {
+    method: 'GET',
+    headers
+  }
+  )
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(data => document.getElementById('output').innerHTML = data.result[0].id)
+    .catch(error => console.error(error)
+    );
+});
+
+// set headers
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: process.env.Token
+};
+
