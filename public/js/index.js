d46a3c6ec844ea9fa58311f7e00c90c6a142d68b
Kip Dunlap <kipling.dunlap@gmail.com>
2023-05-10 16:16:19 -0400

    what's a signature?

--- public/js/index.js
+++ public/js/index.js
@@ -1,49 +1,21 @@
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+document.getElementById('button').addEventListener('click',
+  // Records call
+  function (Token) {
+    // set headers
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", Token);
 
-var urlencoded = new URLSearchParams();
-urlencoded.append("client_id", process.env.client_id);
-urlencoded.append("client_secret", process.env.client_secret);
-urlencoded.append("username", process.env.username);
-urlencoded.append("password", process.env.password);
-urlencoded.append("agency_name", "ATLANTA_GA");
-urlencoded.append("environment", "SUPP");
-urlencoded.append("grant_type", "password");
-urlencoded.append("scope", "records");
+    // set options
+    let requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: urlencoded,
-  redirect: 'follow'
-};
-
-function getToken() {
-  fetch("https://auth.accela.com/oauth2/token", requestOptions)
-    .then(response => response.text())
-    // set token variable
-    .then(result => {
-      var Token = JSON.parse(result).access_token;
-      return Token;
-    })
-    .catch(error => console.log('error', error));
-}
-
-document.getElementById('button').addEventListener('click', function () {
-  getToken();
-  // set headers
-  const headers = {
-    'Content-Type': 'application/json',
-    Authorization: Token
-  }
-  // alert('Hello World!');
-  fetch('https://apis.accela.com/v4/records', {
-    method: 'GET',
-    headers
-  })
-    .then(response => response.json())
-    .then(data => document.getElementById('output').innerHTML = JSON.stringify(data))
-    .then(data => console.log(data))
-    .catch(error => console.error(error)
-    );
-});
+    fetch("https://apis.accela.com/v4/records/", requestOptions)
+      .then(response => response.text())
+      .then(data => document.getElementById('output').innerHTML = data)
+      .then(data => console.log(data))
+      .catch(error => console.error(error)
+      );
+  });
--- server.js
+++ server.js
@@ -20,5 +20,45 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', async (req, res) => {
-  res.render('index');
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("client_id", process.env.client_id);
+  urlencoded.append("client_secret", process.env.client_secret);
+  urlencoded.append("username", process.env.username);
+  urlencoded.append("password", process.env.password);
+  urlencoded.append("agency_name", "ATLANTA_GA");
+  urlencoded.append("environment", "SUPP");
+  urlencoded.append("grant_type", "password");
+  urlencoded.append("scope", "records");
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow'
+  };
+
+  const Token = await fetch("https://auth.accela.com/oauth2/token", requestOptions)
+    .then(response => response.text())
+    // set token variable
+    .then(result => {
+      var Token = JSON.parse(result).access_token;
+      return Token;
+    })
+
+  res.render('index', { Token: Token });
+  console.log(Token);
 });
+
+function getToken() {
+  fetch("https://auth.accela.com/oauth2/token", requestOptions)
+    .then(response => response.text())
+    // set token variable
+    .then(result => {
+      var Token = JSON.parse(result).access_token;
+      return Token;
+    })
+    .catch(error => console.log('error', error));
+}
