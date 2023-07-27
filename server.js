3b825e78f37c12400b57215c2d445616f3dc56b2
Kip Dunlap <kipling.dunlap@gmail.com>
2023-07-27 14:31:58 -0400

    basic address fetch

--- server.js
+++ server.js
@@ -23,13 +23,67 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 // route for ID
-app.get('/test/:id', (req, res) => {
-  console.log(req.params.id);
-
+app.get('/test/:id', async (req, res) => {
   // call accela api with id
-  // TODO: add ID call
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-  res.render('showID', { id: req.params.id });
+  let urlencoded = new URLSearchParams();
+  urlencoded.append("client_id", process.env.CLIENT_ID);
+  urlencoded.append("client_secret", process.env.CLIENT_SECRET);
+  urlencoded.append("username", process.env.ACCELA_USER);
+  urlencoded.append("password", process.env.PASSWORD);
+  urlencoded.append("agency_name", "ATLANTA_GA");
+  urlencoded.append("environment", "SUPP");
+  urlencoded.append("grant_type", "password");
+  urlencoded.append("scope", "records");
+
+  let requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow'
+  };
+
+  // Token is returned from accela auth API
+  const Token = await fetch("https://auth.accela.com/oauth2/token", requestOptions)
+    .then(response => response.text())
+    // .then(result => { console.log(result.status); })
+    // set token variable
+    .then(result => {
+      var Token = JSON.parse(result).access_token;
+      return Token;
+    })
+
+  // Set headers for accela records API call
+  let newHeaders = new Headers();
+  newHeaders.append("Authorization", Token);
+  newHeaders.append("Content-Type", "application/json");
+  newHeaders.append("Connection", "keep-alive");
+  newHeaders.append("Accept", "*/*");
+  newHeaders.append("Accept-Encoding", "gzip, deflate, br");
+
+  // set options
+  let newOptions = {
+    method: 'GET',
+    headers: newHeaders,
+    redirect: 'follow'
+  };
+
+  // Call accela records API with token
+  fetch(`https://apis.accela.com/v4/records/${req.params.id}/addresses`, newOptions)
+    .then(response => response.json())
+    // dump the response to the console
+    .then(result => { console.log(result.result); return result; })
+    // if call is not success, render error page
+    .then(result => {
+      if (result.status !== 200) {
+        res.render('err');
+        return;
+      } else { res.render(`show`, { result: result.result }); }
+    })
+    // .then(result => res.render('test', { result: result }))
+    .catch(error => console.error(error));
 });
 
 // Data page for all records: Call accela Auth API with env vars, then call accela records API with resulting token, passing response to page
@@ -64,8 +118,6 @@ app.get('/test', async (req, res) => {
       return Token;
     })
 
-  // console.log('Token: ' + Token);
-
   // Set headers for accela records API call
   let newHeaders = new Headers();
   newHeaders.append("Authorization", Token);
@@ -87,7 +139,7 @@ app.get('/test', async (req, res) => {
   fetch("https://apis.accela.com/v4/records/", newOptions)
     .then(response => response.json())
     // dump the response to the console
-    .then(result => { console.log(result); return result; })
+    .then(result => { console.log(result.result[0]); return result; })
     // if call is not success, render error page
     .then(result => {
       if (result.status !== 200) {
