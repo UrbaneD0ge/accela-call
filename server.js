124ad9b18012049d4d722945819d656f9215da7e
Kip Dunlap <kipling.dunlap@gmail.com>
2023-07-27 10:46:11 -0400

    add comments to server.js

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Required to pull in environment variables
 dotenv.config();
 
+// Express routes requests to the server to the appropriate pages
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -15,15 +17,22 @@ app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
 
+// 'EJS' template engine
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 // route for ID
-app.get('/test/?...', (req, res) => {
-  res.render('index');
+app.get('/test/:id', (req, res) => {
+  console.log(req.params.id);
+
+  // call accela api with id
+  // TODO: add ID call
+
+  res.render('showID', { id: req.params.id });
 });
 
+// Data page for all records: Call accela Auth API with env vars, then call accela records API with resulting token, passing response to page
 app.get('/test', async (req, res) => {
   let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
@@ -45,6 +54,7 @@ app.get('/test', async (req, res) => {
     redirect: 'follow'
   };
 
+  // Token is returned from accela auth API
   const Token = await fetch("https://auth.accela.com/oauth2/token", requestOptions)
     .then(response => response.text())
     // .then(result => { console.log(result.status); })
@@ -56,6 +66,7 @@ app.get('/test', async (req, res) => {
 
   // console.log('Token: ' + Token);
 
+  // Set headers for accela records API call
   let newHeaders = new Headers();
   newHeaders.append("Authorization", Token);
   newHeaders.append("Content-Type", "application/json");
@@ -72,11 +83,12 @@ app.get('/test', async (req, res) => {
     redirect: 'follow'
   };
 
+  // Call accela records API with token
   fetch("https://apis.accela.com/v4/records/", newOptions)
     .then(response => response.json())
-    // .then(result => { result.replace(/'/g, '"'); return result; })
+    // dump the response to the console
     .then(result => { console.log(result); return result; })
-    // if status code is not 200, show error message
+    // if call is not success, render error page
     .then(result => {
       if (result.status !== 200) {
         res.render('err');
@@ -87,12 +99,12 @@ app.get('/test', async (req, res) => {
     .catch(error => console.error(error));
 });
 
+// In case you really want to see the error page
 app.get('/err', (req, res) => {
   res.render('err');
 });
 
+// landing page
 app.get('/', (req, res) => {
   res.render('index');
 });
-
-// break out test routes
